47d9f8f766cc29a9e6b274338a24f2626b219b79
Eugene Mikhailov <svork@ya.ru>
2023-01-24 10:32:06 +0300

    genhw

--- src/js/GameSaving.js
+++
@@ -1,7 +0,0 @@
-export default class GameSaving {
-  constructor(data) {
-    this.id = data.id;
-    this.created = data.created;
-    this.userInfo = data.userInfo;
-  }
-}
--- src/js/parser.js
+++
@@ -1,7 +0,0 @@
-export default function json(data) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(String.fromCharCode.apply(null, new Uint16Array(data)));
-    }, 500);
-  });
-}
